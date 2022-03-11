6cc49a7b7bf4018a67d61b4d1fd3a6549d5995c4
siva-2001 <slava.kutolvas@gmail.com>
2022-03-12 01:07:51 +0700

    df

--- SportCompetitionService/settings/production.py
+++ SportCompetitionService/settings/production.py
@@ -48,8 +48,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'SCSapp',
-    #'SCSapp.apps.ScsappConfig',
+    #'SCSapp',
+    'SCSapp.apps.ScsappConfig',
 ]
 
 MIDDLEWARE = [
