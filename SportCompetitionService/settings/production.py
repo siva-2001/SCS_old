ae92ad9aa584bce9ad4f3eb68761d0ea660fffb1
siva-2001 <slava.kutolvas@gmail.com>
2022-03-12 01:15:28 +0700

    adsfasd

--- SportCompetitionService/settings/production.py
+++ SportCompetitionService/settings/production.py
@@ -34,7 +34,7 @@ DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 
 ALLOWED_HOSTS = [
-    'https://scs-app.herokuapp.com/'
+    'scs-app.herokuapp.com'
 ]
 
 
@@ -48,8 +48,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #'SCSapp',
-    'SCSapp.apps.ScsappConfig',
+    'SCSapp',
+    #'SCSapp.apps.ScsappConfig',
 ]
 
 MIDDLEWARE = [
