fb03208f247f3499134e2322386162a7e51d465f
siva-2001 <slava.kutolvas@gmail.com>
2022-03-11 23:53:37 +0700

    fixed some moments

--- SportCompetitionService/SportCompetitionService/settings/production.py
+++ SportCompetitionService/SportCompetitionService/settings/production.py
@@ -48,7 +48,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'SCSapp.apps.ScsappConfig',
+    'SCSapp',
+    #'SCSapp.apps.ScsappConfig',
 ]
 
 MIDDLEWARE = [
