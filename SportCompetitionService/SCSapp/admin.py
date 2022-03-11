0825d0bc677656e808b509cea68473f6dd66d7f2
siva-2001 <slava.kutolvas@gmail.com>
2022-03-11 23:56:56 +0700

    change root dir

--- SportCompetitionService/SCSapp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Player, VolleyballTeam, Competition, Match, MatchEvent
-
-admin.site.register(Player)
-admin.site.register(VolleyballTeam)
-admin.site.register(Competition)
-admin.site.register(Match)
-admin.site.register(MatchEvent)
--- SportCompetitionService/SCSapp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ScsappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'SCSapp'
