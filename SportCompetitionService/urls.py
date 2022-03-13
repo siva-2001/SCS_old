419d74a49bce11817c3562d8aeb896275ee0696f
SvyatS <prisenko2002@gmail.com>
2022-03-13 15:04:29 +0700

    feat(): added static root

--- SportCompetitionService/urls.py
+++ SportCompetitionService/urls.py
@@ -32,4 +32,4 @@ urlpatterns = [
     path('competition/<comp_id>/', views.competitionView, name='competition'),
 ]
 
-#urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
